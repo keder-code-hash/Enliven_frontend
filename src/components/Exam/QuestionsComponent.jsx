c286d4e6dc59dfcc90d3a4fd874e36430940ff8a
Keder Nath mallick <53966650+keder-code-hash@users.noreply.github.com>
2022-08-21 22:01:11 +0530

    integrated editor in exam portal

--- src/components/Exam/QuestionsComponent.jsx
+++ src/components/Exam/QuestionsComponent.jsx
@@ -7,8 +7,7 @@ class Questions extends Component {
             <div style={{boxShadow: "rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset", padding: "15px", borderRadius:"8px", marginTop:"2rem"}}>
                 <strong>Q: {this.props.question}</strong>
                 <br/>
-                <br/>
-                <textarea></textarea>
+                <br/> 
             </div>
         );
     }
--- src/stores.js
+++ src/stores.js
@@ -1,13 +1,13 @@
 import { createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./redux/reducers/rootReducers";
-import { composeWithDevTools } from "redux-devtools-extension";
-const middleware = [thunk];
+// import { composeWithDevTools } from "redux-devtools-extension";
+// const middleware = [thunk];
 const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk),
-    composeWithDevTools(applyMiddleware(...middleware))
+    // composeWithDevTools(applyMiddleware(...middleware))
   )
 );
 
