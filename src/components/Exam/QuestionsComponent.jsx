4e5675beded5ec51dd0a97f7582a3e6d8add3adc
Purnadip Manna <purnadipmanna2702@gmail.com>
2022-08-22 21:31:32 +0530

    Exam & Profile page modified

--- src/components/Exam/QuestionsComponent.jsx
+++ src/components/Exam/QuestionsComponent.jsx
@@ -4,11 +4,11 @@ class Questions extends Component {
     state = {  } 
     render() { 
         return (
-            <div style={{boxShadow: "rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset", padding: "15px", borderRadius:"8px", marginTop:"2rem"}}>
+            <React.Fragment>
                 <strong>Q: {this.props.question}</strong>
                 <br/>
                 <br/> 
-            </div>
+            </React.Fragment>
         );
     }
 }
