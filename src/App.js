31ff451fde3e3ed5fa455837f7b05c15123ca793
Ishika Ghosh <idofishika321@gmail.com>
2022-10-22 13:21:26 +0530

    routing implemented

--- src/App.js
+++ src/App.js
@@ -1,64 +1,47 @@
-import './App.css';
-import './css/bee.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import "./css/bee.css";
 
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Testing from './pages/Testing';
-import Dashboard from './pages/Dashboard';
-import About from './pages/About';
-import Notfound from './pages/Notfound';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Testing from "./pages/Testing";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
+import Notfound from "./pages/Notfound";
 
-import Header from './components/Utils/Header';
-import Footer from './components/Utils/Footer';
-import ExamPage from './components/Exam/ExamPage';
-import Profile from './components/Dashboard/ProfileComponent';
+import Header from "./components/Utils/Header";
+import Footer from "./components/Utils/Footer";
+import ExamPage from "./components/Exam/ExamPage";
+import Profile from "./components/Dashboard/ProfileComponent";
 
 function App() {
-  let component;
-  switch (window.location.pathname) {
-    case '/':
-      component = <Home/>
-      break;
-    
-    case '/login':
-      component = <Login/>
-      break;
-
-    case '/signup':
-      component = <Signup/>
-      break;  
-
-    case '/dashboard':
-      component = <Dashboard title="Student"/>
-      break;
-
-    case '/testing':
-      component = <Testing/>
-      break;
-
-    case '/about':
-      component = <About/>
-      break;
-
-    case '/exam':
-      component = <ExamPage examname="Computer Science" fullmarks="10" time="00:10:00"/>
-      break;
-
-
-    case '/profile':
-      component = <Profile/>
-      break;
-
-    default:
-      component = <Notfound/>
-      break;
-  }
   return (
     <>
-      <Header/>
-      {component}
-      <Footer/>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/testing" element={<Testing />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route
+            path="/exam"
+            element={
+              <ExamPage
+                examname="Computer Science"
+                fullmarks="10"
+                time="00:10:00"
+              />
+            }
+          />
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
