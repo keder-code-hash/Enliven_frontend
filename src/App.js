37875b52dc17554899785e0e846f2fdb50000de5
Ishika Ghosh <idofishika321@gmail.com>
2022-10-23 16:15:59 +0530

    navbar design completed

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
           />
           <Route path="*" element={<Notfound />} />
         </Routes>
-        <Footer />
+        {/* <Footer /> */}
       </BrowserRouter>
     </>
   );
